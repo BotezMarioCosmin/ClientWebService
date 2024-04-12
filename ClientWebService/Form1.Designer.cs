24bc6d8b7b3c04bfc037071c0e46023fb7b0a201
Botez.19313 <Botez.19313@LABI1-6PC19>
2024-04-12 10:33:46 +0200

    Aggiungere i file di progetto.

---
+++ ClientWebService/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ClientWebService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnPost = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(223, 149);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 0;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(364, 149);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 1;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(293, 71);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 2;
+            this.btnGet.Text = "GET";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // btnPost
+            // 
+            this.btnPost.Location = new System.Drawing.Point(389, 71);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(75, 23);
+            this.btnPost.TabIndex = 3;
+            this.btnPost.Text = "POST";
+            this.btnPost.UseVisualStyleBackColor = true;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPost);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnPost;
+    }
+}
+
---
+++ ClientWebService/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace ClientWebService
+{
+    public partial class Form1 : Form
+    {
+        private const string BaseUrl = "http://localhost/phpmyadmin";
+
+        private HttpClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new HttpClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnGet_Click(object sender, EventArgs e)
+        {
+            HttpResponseMessage response = await client.GetAsync(BaseUrl + "resource");
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Gestione del codice di risposta
+            if (response.IsSuccessStatusCode)
+            {
+                // Conversione da JSON a oggetto
+                var data = JsonConvert.DeserializeObject(responseBody);
+                // Rappresentazione grafica dei dati
+                // Esempio: dataGridView.DataSource = data;
+            }
+            else
+            {
+                MessageBox.Show("Errore: " + response.StatusCode);
+            }
+        }
+
+        private async void btnPost_Click(object sender, EventArgs e)
+        {
+            var data = new { a = Int32.Parse(txtA.Text), b = Int32.Parse(txtB.Text) };
+            string json = JsonConvert.SerializeObject(data);
+
+            var content = new StringContent(json);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            HttpResponseMessage response = await client.PostAsync(BaseUrl + "resource", content);
+
+            // Gestione del codice di risposta
+            if (response.IsSuccessStatusCode)
+            {
+                // Esegue operazioni se necessario
+            }
+            else
+            {
+                MessageBox.Show("Errore: " + response.StatusCode);
+            }
+        }
+    }
+}
