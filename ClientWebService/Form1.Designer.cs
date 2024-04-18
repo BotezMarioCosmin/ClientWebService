13a2540d1bf69fcea70905adfdcc43e508707eb3
Botez Mario Cosmin <botezmariocosmin@gmail.com>
2024-04-18 21:30:47 +0200

    aggiunta metodo post

--- ClientWebService/Form1.Designer.cs
+++ ClientWebService/Form1.Designer.cs
@@ -28,33 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtA = new System.Windows.Forms.TextBox();
-            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtRecord = new System.Windows.Forms.TextBox();
+            this.txtPostPrezzo = new System.Windows.Forms.TextBox();
             this.btnGet = new System.Windows.Forms.Button();
             this.btnPost = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPostNome = new System.Windows.Forms.TextBox();
+            this.txtPostCategoria = new System.Windows.Forms.TextBox();
+            this.txtPostSviluppatore = new System.Windows.Forms.TextBox();
+            this.txtPostPubblicatore = new System.Windows.Forms.TextBox();
+            this.pnlPost = new System.Windows.Forms.Panel();
+            this.lblPost5 = new System.Windows.Forms.Label();
+            this.lblPost4 = new System.Windows.Forms.Label();
+            this.lblPost3 = new System.Windows.Forms.Label();
+            this.lblPost2 = new System.Windows.Forms.Label();
+            this.lblPost1 = new System.Windows.Forms.Label();
+            this.lblPost = new System.Windows.Forms.Label();
+            this.btnPostIndietro = new System.Windows.Forms.Button();
+            this.btnApriPost = new System.Windows.Forms.Button();
+            this.btnApriPut = new System.Windows.Forms.Button();
+            this.lblPut = new System.Windows.Forms.Label();
+            this.btnPut = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pnlPost.SuspendLayout();
             this.SuspendLayout();
             // 
-            // txtA
+            // txtRecord
             // 
-            this.txtA.Location = new System.Drawing.Point(12, 41);
-            this.txtA.Name = "txtA";
-            this.txtA.Size = new System.Drawing.Size(100, 20);
-            this.txtA.TabIndex = 0;
+            this.txtRecord.Location = new System.Drawing.Point(12, 41);
+            this.txtRecord.Name = "txtRecord";
+            this.txtRecord.Size = new System.Drawing.Size(134, 20);
+            this.txtRecord.TabIndex = 0;
             // 
-            // txtB
+            // txtPostPrezzo
             // 
-            this.txtB.Location = new System.Drawing.Point(118, 41);
-            this.txtB.Name = "txtB";
-            this.txtB.Size = new System.Drawing.Size(100, 20);
-            this.txtB.TabIndex = 1;
+            this.txtPostPrezzo.Location = new System.Drawing.Point(96, 75);
+            this.txtPostPrezzo.Name = "txtPostPrezzo";
+            this.txtPostPrezzo.Size = new System.Drawing.Size(100, 20);
+            this.txtPostPrezzo.TabIndex = 1;
             // 
             // btnGet
             // 
             this.btnGet.Location = new System.Drawing.Point(12, 12);
             this.btnGet.Name = "btnGet";
-            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.Size = new System.Drawing.Size(134, 23);
             this.btnGet.TabIndex = 2;
             this.btnGet.Text = "GET";
             this.btnGet.UseVisualStyleBackColor = true;
@@ -62,36 +82,222 @@
             // 
             // btnPost
             // 
-            this.btnPost.Location = new System.Drawing.Point(93, 12);
+            this.btnPost.Location = new System.Drawing.Point(112, 179);
             this.btnPost.Name = "btnPost";
-            this.btnPost.Size = new System.Drawing.Size(75, 23);
+            this.btnPost.Size = new System.Drawing.Size(84, 23);
             this.btnPost.TabIndex = 3;
-            this.btnPost.Text = "POST";
+            this.btnPost.Text = "Esegui POST";
             this.btnPost.UseVisualStyleBackColor = true;
             this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 76);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 98);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 362);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 340);
             this.dataGridView1.TabIndex = 4;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(363, 41);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(134, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(12, 64);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(85, 13);
+            this.lbl1.TabIndex = 6;
+            this.lbl1.Text = "Inserire id record";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Lascia vuoto per get all";
+            // 
+            // txtPostNome
+            // 
+            this.txtPostNome.Location = new System.Drawing.Point(96, 49);
+            this.txtPostNome.Name = "txtPostNome";
+            this.txtPostNome.Size = new System.Drawing.Size(100, 20);
+            this.txtPostNome.TabIndex = 8;
+            // 
+            // txtPostCategoria
+            // 
+            this.txtPostCategoria.Location = new System.Drawing.Point(96, 101);
+            this.txtPostCategoria.Name = "txtPostCategoria";
+            this.txtPostCategoria.Size = new System.Drawing.Size(100, 20);
+            this.txtPostCategoria.TabIndex = 9;
+            // 
+            // txtPostSviluppatore
+            // 
+            this.txtPostSviluppatore.Location = new System.Drawing.Point(96, 127);
+            this.txtPostSviluppatore.Name = "txtPostSviluppatore";
+            this.txtPostSviluppatore.Size = new System.Drawing.Size(100, 20);
+            this.txtPostSviluppatore.TabIndex = 10;
+            // 
+            // txtPostPubblicatore
+            // 
+            this.txtPostPubblicatore.Location = new System.Drawing.Point(96, 153);
+            this.txtPostPubblicatore.Name = "txtPostPubblicatore";
+            this.txtPostPubblicatore.Size = new System.Drawing.Size(100, 20);
+            this.txtPostPubblicatore.TabIndex = 11;
+            // 
+            // pnlPost
+            // 
+            this.pnlPost.Controls.Add(this.btnPut);
+            this.pnlPost.Controls.Add(this.btnPostIndietro);
+            this.pnlPost.Controls.Add(this.lblPost);
+            this.pnlPost.Controls.Add(this.lblPost5);
+            this.pnlPost.Controls.Add(this.lblPost4);
+            this.pnlPost.Controls.Add(this.lblPost3);
+            this.pnlPost.Controls.Add(this.lblPost2);
+            this.pnlPost.Controls.Add(this.lblPost1);
+            this.pnlPost.Controls.Add(this.btnPost);
+            this.pnlPost.Controls.Add(this.txtPostPubblicatore);
+            this.pnlPost.Controls.Add(this.txtPostNome);
+            this.pnlPost.Controls.Add(this.txtPostSviluppatore);
+            this.pnlPost.Controls.Add(this.txtPostPrezzo);
+            this.pnlPost.Controls.Add(this.txtPostCategoria);
+            this.pnlPost.Location = new System.Drawing.Point(279, 121);
+            this.pnlPost.Name = "pnlPost";
+            this.pnlPost.Size = new System.Drawing.Size(218, 219);
+            this.pnlPost.TabIndex = 13;
+            // 
+            // lblPost5
+            // 
+            this.lblPost5.AutoSize = true;
+            this.lblPost5.Location = new System.Drawing.Point(21, 156);
+            this.lblPost5.Name = "lblPost5";
+            this.lblPost5.Size = new System.Drawing.Size(69, 13);
+            this.lblPost5.TabIndex = 23;
+            this.lblPost5.Text = "Pubblicatore:";
+            // 
+            // lblPost4
+            // 
+            this.lblPost4.AutoSize = true;
+            this.lblPost4.Location = new System.Drawing.Point(21, 130);
+            this.lblPost4.Name = "lblPost4";
+            this.lblPost4.Size = new System.Drawing.Size(69, 13);
+            this.lblPost4.TabIndex = 22;
+            this.lblPost4.Text = "Sviluppatore:";
+            // 
+            // lblPost3
+            // 
+            this.lblPost3.AutoSize = true;
+            this.lblPost3.Location = new System.Drawing.Point(35, 104);
+            this.lblPost3.Name = "lblPost3";
+            this.lblPost3.Size = new System.Drawing.Size(55, 13);
+            this.lblPost3.TabIndex = 21;
+            this.lblPost3.Text = "Categoria:";
+            // 
+            // lblPost2
+            // 
+            this.lblPost2.AutoSize = true;
+            this.lblPost2.Location = new System.Drawing.Point(48, 78);
+            this.lblPost2.Name = "lblPost2";
+            this.lblPost2.Size = new System.Drawing.Size(42, 13);
+            this.lblPost2.TabIndex = 20;
+            this.lblPost2.Text = "Prezzo:";
+            // 
+            // lblPost1
+            // 
+            this.lblPost1.AutoSize = true;
+            this.lblPost1.Location = new System.Drawing.Point(52, 52);
+            this.lblPost1.Name = "lblPost1";
+            this.lblPost1.Size = new System.Drawing.Size(38, 13);
+            this.lblPost1.TabIndex = 19;
+            this.lblPost1.Text = "Nome:";
+            // 
+            // lblPost
+            // 
+            this.lblPost.AutoSize = true;
+            this.lblPost.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPost.Location = new System.Drawing.Point(20, 11);
+            this.lblPost.Name = "lblPost";
+            this.lblPost.Size = new System.Drawing.Size(187, 24);
+            this.lblPost.TabIndex = 24;
+            this.lblPost.Text = "Inserisci valori record";
+            // 
+            // btnPostIndietro
+            // 
+            this.btnPostIndietro.Location = new System.Drawing.Point(24, 179);
+            this.btnPostIndietro.Name = "btnPostIndietro";
+            this.btnPostIndietro.Size = new System.Drawing.Size(84, 23);
+            this.btnPostIndietro.TabIndex = 25;
+            this.btnPostIndietro.Text = "Indietro";
+            this.btnPostIndietro.UseVisualStyleBackColor = true;
+            this.btnPostIndietro.Click += new System.EventHandler(this.btnPostIndietro_Click);
+            // 
+            // btnApriPost
+            // 
+            this.btnApriPost.Location = new System.Drawing.Point(190, 12);
+            this.btnApriPost.Name = "btnApriPost";
+            this.btnApriPost.Size = new System.Drawing.Size(134, 23);
+            this.btnApriPost.TabIndex = 14;
+            this.btnApriPost.Text = "POST";
+            this.btnApriPost.UseVisualStyleBackColor = true;
+            this.btnApriPost.Click += new System.EventHandler(this.btnApriPost_Click);
+            // 
+            // btnApriPut
+            // 
+            this.btnApriPut.Location = new System.Drawing.Point(363, 12);
+            this.btnApriPut.Name = "btnApriPut";
+            this.btnApriPut.Size = new System.Drawing.Size(134, 23);
+            this.btnApriPut.TabIndex = 15;
+            this.btnApriPut.Text = "PUT";
+            this.btnApriPut.UseVisualStyleBackColor = true;
+            this.btnApriPut.Click += new System.EventHandler(this.btnApriPut_Click);
+            // 
+            // lblPut
+            // 
+            this.lblPut.AutoSize = true;
+            this.lblPut.Location = new System.Drawing.Point(360, 64);
+            this.lblPut.Name = "lblPut";
+            this.lblPut.Size = new System.Drawing.Size(85, 13);
+            this.lblPut.TabIndex = 16;
+            this.lblPut.Text = "Inserire id record";
+            // 
+            // btnPut
+            // 
+            this.btnPut.Location = new System.Drawing.Point(112, 179);
+            this.btnPut.Name = "btnPut";
+            this.btnPut.Size = new System.Drawing.Size(84, 23);
+            this.btnPut.TabIndex = 26;
+            this.btnPut.Text = "Esegui PUT";
+            this.btnPut.UseVisualStyleBackColor = true;
+            this.btnPut.Click += new System.EventHandler(this.btnPut_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPut);
+            this.Controls.Add(this.btnApriPut);
+            this.Controls.Add(this.btnApriPost);
+            this.Controls.Add(this.pnlPost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtId);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnPost);
             this.Controls.Add(this.btnGet);
-            this.Controls.Add(this.txtB);
-            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtRecord);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.pnlPost.ResumeLayout(false);
+            this.pnlPost.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,11 +305,30 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtA;
-        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtRecord;
+        private System.Windows.Forms.TextBox txtPostPrezzo;
         private System.Windows.Forms.Button btnGet;
         private System.Windows.Forms.Button btnPost;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPostNome;
+        private System.Windows.Forms.TextBox txtPostCategoria;
+        private System.Windows.Forms.TextBox txtPostSviluppatore;
+        private System.Windows.Forms.TextBox txtPostPubblicatore;
+        private System.Windows.Forms.Panel pnlPost;
+        private System.Windows.Forms.Label lblPost5;
+        private System.Windows.Forms.Label lblPost4;
+        private System.Windows.Forms.Label lblPost3;
+        private System.Windows.Forms.Label lblPost2;
+        private System.Windows.Forms.Label lblPost1;
+        private System.Windows.Forms.Button btnPostIndietro;
+        private System.Windows.Forms.Label lblPost;
+        private System.Windows.Forms.Button btnApriPost;
+        private System.Windows.Forms.Button btnApriPut;
+        private System.Windows.Forms.Label lblPut;
+        private System.Windows.Forms.Button btnPut;
     }
 }
 
--- ClientWebService/Form1.cs
+++ ClientWebService/Form1.cs
@@ -15,6 +15,16 @@ namespace ClientWebService
 {
     public partial class Form1 : Form
     {
+        public class Prodotto 
+        {
+            string Prodottoid;
+            string nome;
+            string prezzo;
+            string categoria;
+            string sviluppatore;
+            string pubblicatore;
+        }
+
         private const string BaseUrl = "http://localhost/wsphp/index.php/prodotti";
 
         private HttpClient client;
@@ -22,6 +32,7 @@ namespace ClientWebService
         {
             InitializeComponent();
             client = new HttpClient();
+            pnlPost.Hide();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -29,17 +40,30 @@ namespace ClientWebService
 
         }
 
+
         private async void btnGet_Click(object sender, EventArgs e)
         {
-            HttpResponseMessage response = await client.GetAsync(BaseUrl);// + "resource");
-            string responseBody = await response.Content.ReadAsStringAsync();
+            HttpResponseMessage response;
+            string responseBody;
 
-            // Gestione del codice di risposta
+            if (txtRecord.Text == "")
+            {
+                response = await client.GetAsync(BaseUrl);
+                responseBody = await response.Content.ReadAsStringAsync();
+            }
+            else 
+            {
+                response = await client.GetAsync((BaseUrl) + "/" + txtRecord.Text);
+                responseBody = await response.Content.ReadAsStringAsync();
+            }
+
+            //codice risposta
             if (response.IsSuccessStatusCode)
             {
-                // Conversione da JSON a oggetto
+                MessageBox.Show(responseBody);
+                //conversione
                 var data = JsonConvert.DeserializeObject(responseBody);
-                // Rappresentazione grafica dei dati
+                //visualizza
                 dataGridView1.DataSource = data;
                 MessageBox.Show("GET eseguito con successo: " + response.StatusCode);
             }
@@ -51,15 +75,56 @@ namespace ClientWebService
 
         private async void btnPost_Click(object sender, EventArgs e)
         {
-            var data = new { a = Int32.Parse(txtA.Text), b = Int32.Parse(txtB.Text) };
+            if (txtPostNome.Text != null && txtPostPrezzo.Text != null && txtPostCategoria.Text != null && txtPostSviluppatore.Text != null && txtPostPubblicatore.Text != null )
+            {
+                //crea aggetto
+                var data = new { nome = txtPostNome.Text, prezzo = txtPostPrezzo.Text, categoria = txtPostCategoria.Text, sviluppatore = txtPostSviluppatore.Text, pubblicatore = txtPostPubblicatore.Text };
+
+                
+                var settings = new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                };
+
+                //converti in json
+                string json = JsonConvert.SerializeObject(data, settings);
+
+                var content = new StringContent(json);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                //richiesta post
+                HttpResponseMessage response = await client.PostAsync(BaseUrl + "/add", content);
+
+                //risposta
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Record aggiunto con successo:" + response.StatusCode);
+                }
+                else
+                {
+                    MessageBox.Show("Errore: " + response.StatusCode);
+                }
+                pnlPost.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Campi non validi.");
+            }
+            
+        }
+
+
+        private async void btnPut_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var data = new { a = Int32.Parse(txtRecord.Text), b = Int32.Parse(txtPostPrezzo.Text) };
             string json = JsonConvert.SerializeObject(data);
 
             var content = new StringContent(json);
             content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-            HttpResponseMessage response = await client.PostAsync(BaseUrl + "resource", content);
+            HttpResponseMessage response = await client.PutAsync($"{BaseUrl}/put/{id}", content);
 
-            // Gestione del codice di risposta
             if (response.IsSuccessStatusCode)
             {
                 // Esegue operazioni se necessario
@@ -67,6 +132,49 @@ namespace ClientWebService
             else
             {
                 MessageBox.Show("Errore: " + response.StatusCode);
+            }
+            txtId.Clear();
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text); // Assumi che l'ID dell'elemento da eliminare sia inserito in un campo di testo chiamato txtId
+
+            HttpResponseMessage response = await client.DeleteAsync($"{BaseUrl}/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Esegue operazioni se necessario
+            }
+            else
+            {
+                MessageBox.Show("Errore: " + response.StatusCode);
+            }
+        }
+
+        private void btnApriPost_Click(object sender, EventArgs e)
+        {
+            pnlPost.Show();
+            btnPost.Show();
+            btnPut.Hide();
+        }
+
+        private void btnPostIndietro_Click(object sender, EventArgs e)
+        {
+            pnlPost.Hide();
+        }
+
+        private void btnApriPut_Click(object sender, EventArgs e)
+        {
+            btnPost.Hide();
+            btnPut.Show();
+            if (txtId.Text != "" && int.TryParse(txtId.Text, out int id))
+            {
+                pnlPost.Show();
+            }
+            else
+            {
+                MessageBox.Show("Bisogna specificare un id numerico.");
             }
         }
     }
